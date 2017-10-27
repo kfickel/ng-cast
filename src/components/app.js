c6507072257d417a9bd05b7ca502c776362edf73
Hack Reactor Student <student@hackreactor.com>
2017-10-27 15:31:52 -0700

    Got App to render

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,16 @@
 angular.module('video-player')
 
 .component('app', {
+
   // TODO
+  controller: function() {
+    this.selectVideo = () => {
+    };
+    this.searchResults = () => {
+    };
+    this.currentVideo = {};
+    this.videos = [];
+  },
+
+  templateUrl: 'src/templates/app.html'
 });
